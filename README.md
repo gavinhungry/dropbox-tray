a031f00ffb5448d9fadb4aa6bbf50d8373cf9133
Gavin Lloyd <gavinhungry@gmail.com>
2017-12-13 12:00:59 -0800

    Replace NW.js implementation with Python3/GTK

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 dropbox-tray
 ============
-A simple Dropbox tray icon using [NW.js](https://github.com/nwjs/nw.js).
+A Dropbox tray icon built with Python and GTK.
 
 Usage
 -----
 
-    $ npm install
-    $ nw .
+    $ ./dropbox-tray.py
+
+Attributions
+------------
+The default icons found in `icons/` are from
+[Faenza Icons](https://tiheum.deviantart.com/art/Faenza-Icons-173323228).
 
 License
 -------
---
+++ dropbox-tray.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# dropbox-tray: Dropbox tray icon
+# https://github.com/gavinhungry/dropbox-tray
+#
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GObject
+
+from enum import Enum
+import json
+import os
+import signal
+import subprocess
+
+class DropboxTray:
+  class Icon(Enum):
+    OFFLINE = 'icons/offline.png'
+    IDLE = 'icons/idle.png'
+    BUSY = 'icons/busy.png'
+    BUSY2 = 'icons/busy2.png'
+    ERROR = 'icons/error.png'
+
+  class Status(Enum):
+    IDLE = 'Up to date'
+    INDEXING = 'Indexing'
+    OFFLINE = "Dropbox isn't running!"
+    SYNCING = 'Syncing'
+    UPLOADING = 'Uploading'
+
+  def __init__(self):
+    self.directory = self.getDirectory()
+    self.initIcon()
+    Gtk.main()
+
+  def getDirectory(self):
+    return json.load(open('/home/gavin/.dropbox/info.json'))['personal']['path']
+
+  def openDirectory(self, widget):
+    subprocess.Popen(['xdg-open', self.directory])
+
+  def initIcon(self):
+    self.icon = Gtk.StatusIcon()
+    self.setIcon(self.Icon.OFFLINE)
+    self.icon.connect('activate', self.openDirectory)
+    self.icon.timeout = GObject.timeout_add(500, self.update)
+    self.icon.set_visible(True)
+
+  def setIcon(self, icon):
+    self.icon.attr = icon
+    self.icon.set_from_file(icon.value)
+
+  def setTitle(self, title):
+    if self.icon.get_tooltip_text() != title:
+      self.icon.set_tooltip_text(title)
+
+  def setIconBusy(self):
+    self.setIcon(self.Icon.BUSY2 if self.icon.attr == self.Icon.BUSY else self.Icon.BUSY)
+
+  def getStatuses(self):
+    return list(filter(None, os.popen('dropbox-cli status').read().split('\n')))
+
+  def getStatus(self):
+    statuses = self.getStatuses()
+
+    if not len (statuses):
+      return 'Dropbox'
+
+    filtered = list(filter(lambda s:
+      str.startswith(s, self.Status.UPLOADING.value) or
+      str.startswith(s, self.Status.SYNCING.value) or
+      str.startswith(s, self.Status.INDEXING.value),
+    statuses))
+
+    return filtered[0] if len(filtered) else statuses[0]
+
+  def isRunning(self):
+    return self.getStatus() != self.Status.OFFLINE.value
+
+  def isBusy(self):
+    return self.isRunning() and self.getStatus() != self.Status.IDLE.value
+
+  def update(self):
+    self.setTitle(self.getStatus())
+
+    if self.isBusy():
+      self.setIconBusy()
+    else:
+      self.setIcon(self.Icon.IDLE if self.isRunning() else self.Icon.OFFLINE)
+
+    return True
+
+if __name__ == '__main__':
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+  DropboxTray()
